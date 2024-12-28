a52c0a3f18ef11e18db9c80a927f4bd147b4000b
LisadiKaprio <lisadikaprio@gmail.com>
2024-12-28 12:07:06 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8080,
+    hot: true
+  },
+};
